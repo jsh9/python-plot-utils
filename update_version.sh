db0e3c80ee9b13db934b560387b73b3219dd9c7d
jsh9 <25124332+jsh9@users.noreply.github.com>
2022-05-15 23:20:43 -0700

    Update sed command (per https://stackoverflow.com/a/19457213)

--- update_version.sh
+++ update_version.sh
@@ -1,7 +1,7 @@
 echo "Updating version number in files: from $1 to $2"
-sed -i "s/$1/$2/g" README.md
-sed -i "s/$1/$2/g" setup.py
-sed -i "s/$1/$2/g" ./doc/source/index.rst
-sed -i "s/$1/$2/g" ./doc/source/conf.py
-sed -i "s/$1/$2/g" ./doc/source/installation_guide.rst
-sed -i "s/$1/$2/g" ./plot_utils/__init__.py
+sed -i "" -e "s/$1/$2/g" README.md
+sed -i "" -e "s/$1/$2/g" setup.py
+sed -i "" -e "s/$1/$2/g" ./doc/source/index.rst
+sed -i "" -e "s/$1/$2/g" ./doc/source/conf.py
+sed -i "" -e "s/$1/$2/g" ./doc/source/installation_guide.rst
+sed -i "" -e "s/$1/$2/g" ./plot_utils/__init__.py

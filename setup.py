af9d1d06c285dc06a83c884392b6cefa855f53ba
jsh9 <25124332+jsh9@users.noreply.github.com>
2023-05-01 00:32:21 -0700

    Migrate to setup.cfg

--- setup.py
+++ setup.py
@@ -1,32 +1,4 @@
 from setuptools import setup
 
-setup(
-    name='plot_utils',
-    version='v0.6.14',
-    description='A Python library for elegant data visualization',
-    author='Jian Shi',
-    license='GPL v3.0',
-    url='https://github.com/jsh9/python-plot-utils',
-    packages = ['plot_utils'],
-    classifiers = [
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Visualization',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-    ],
-    install_requires=[
-        'numpy>=1.11.0',
-        'scipy>=0.19.0',
-        'pandas>=0.17.1',
-        'cycler>=0.10.0',
-        'matplotlib',
-    ],
-    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
-    include_package_data=True,
-)
+
+setup()
